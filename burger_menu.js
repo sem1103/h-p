58dabe76619139c14a0f52fe399ba82e29216676
sem1103 <65673890+sem1103@users.noreply.github.com>
2021-05-16 12:55:24 +0400

    Add files via upload

---
+++ burger_menu.js
@@ -0,0 +1,24 @@
+let open = document.getElementById('open');
+let close = document.getElementById('close');
+let nav = document.getElementsByTagName('nav');
+let menuList = document.querySelector('#burger__menu> div:last-child');
+let li = document.querySelector('#burger__menu').getElementsByTagName('li')
+for(let item in li){
+    li[item].onclick = function(){
+        closeMenu();
+    };
+}
+function openMenu(){
+    nav[0].setAttribute('style', 'background: #1a1302b6;z-index: 1;');
+    open.setAttribute('style', 'transform:translateX(-100%); opacity:0;')
+    close.setAttribute('style','transform:translateX(0); opacity:1;')
+    menuList.setAttribute('style','transform:translateX(0); ');
+   
+    
+}
+function closeMenu(){
+    nav[0].setAttribute('style', 'background: none;z-index: 0;');
+    open.setAttribute('style', 'transform:translateX(-0%); opacity:1;')
+    close.setAttribute('style','transform:translateX(-100%); opacity:0;');
+    menuList.setAttribute('style','transform:translateX(-100%); ');
+}
